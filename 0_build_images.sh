ae4e980369ec5cd344eafca39171ea858f150876
doug <doug.burks@gmail.com>
2019-03-12 12:59:57 -0400

    bump VERSION to 6.6.2

--- 0_build_images.sh
+++ 0_build_images.sh
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Set defaults and allow overriding via conf file
-VERSION=6.6.1
+VERSION=6.6.2
 FLAVOR="-oss"
 DOCKERHUB="securityonionsolutionstest"
 [ $# -eq 1 ] && [ -f $1 ] && source $1
