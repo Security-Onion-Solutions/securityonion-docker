571683d4c3a5ad3c2a05ef9824ef53064e0f6ad3
doug <doug.burks@gmail.com>
2020-03-18 11:10:07 -0400

    increment version to 6.8.7

--- 0_build_images.sh
+++ 0_build_images.sh
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Set defaults and allow overriding via conf file
-VERSION=6.8.6
+VERSION=6.8.7
 FLAVOR="-oss"
 DOCKERHUB="securityonionsolutionstest"
 [ $# -eq 1 ] && [ -f $1 ] && source $1
